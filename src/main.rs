095c248610d34ad2b1c391698120352562e93a39
andrewwillette <willette.andrew1846@gmail.com>
2023-05-21 19:44:28 -0500

    master: moving music_page function

--- src/main.rs
+++ src/main.rs
@@ -145,18 +145,6 @@ impl Props {
     }
 }
 
-#[function_component(Music)]
-fn music_page(props: &Props) -> Html {
-    html! {
-    <section>
-        <ul>
-            // <li onclick={move|_|{home_click_handler.emit("home".to_string());}}>{"Home"}</li>
-            <div>{"listen to music"}</div>
-        </ul>
-    </section>
-        }
-}
-
 #[function_component(CVPage)]
 fn cv_page(props: &Props) -> Html {
     html! {
