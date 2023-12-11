d911e760002b503212778de29c384e7eb93d3be2
andrewwillette <willette.andrew1846@gmail.com>
2023-12-10 19:31:17 -0600

    master: building out home page

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -1,10 +1,14 @@
 use yew::prelude::*;
 pub struct Home;
 
+const HOME_BIO: &str =
+    " Hi! My name is Andrew Willette. I am a software developer based in Madison, Wisconsin.";
+
+const HOME_BIO_CONT: &str = "I like playing violin and host some recordings on my site here.";
+// I like playing violin and host some recordings on my site here.
 impl Component for Home {
     type Message = ();
     type Properties = ();
-
     fn create(_ctx: &Context<Self>) -> Self {
         Self
     }
@@ -13,8 +17,10 @@ impl Component for Home {
         html! {
             <div class="tile is-ancestor is-vertical">
                 <div class="tile is-child hero">
-                    <div class="hero-body container pb-0">
-                        <h1 class="title is-1">{ "Home Page" }</h1>
+                    <div class="home-container pb-0">
+                        <img class="center" src="https://andrewwillette.s3.us-east-2.amazonaws.com/newdir/website_photo_2.jpeg" alt="Homepage Image" />
+                        <p class="center">{ HOME_BIO }</p>
+                        <p class="center">{ HOME_BIO_CONT }</p>
                     </div>
                 </div>
             </div>
